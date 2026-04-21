a658cb9989103d3fd19df88cf540e5b38f85d5cf
darbinyanharut48-s <darbinyanharut48@gmail.com>
2026-04-21 10:44:01 +0400

    add sitemap

--- server/routes/public.js
+++ server/routes/public.js
@@ -10,6 +10,29 @@ function asyncHandler(handler) {
   return (req, res, next) => Promise.resolve(handler(req, res, next)).catch(next);
 }
 
+function buildAbsoluteUrl(pathname = '/') {
+  const normalized = pathname.startsWith('/') ? pathname : `/${pathname}`;
+  return `${env.siteUrl}${normalized}`;
+}
+
+function toIsoDate(value) {
+  const date = new Date(value || Date.now());
+  if (Number.isNaN(date.getTime())) {
+    return new Date().toISOString();
+  }
+
+  return date.toISOString();
+}
+
+function escapeXml(value = '') {
+  return String(value)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+}
+
 router.get('/', (_req, res) => {
   res.sendFile(path.join(env.paths.publicDir, 'index.html'));
 });
@@ -27,5 +50,62 @@ router.get('/blog/:slug', asyncHandler(async (req, res, next) => {
 
   return res.send(renderBlogDetailPage(post));
 }));
+
+router.get('/sitemap.xml', asyncHandler(async (_req, res) => {
+  const posts = await listPublishedPosts();
+  const latestPostDate = posts.length
+    ? posts[0].updated_at || posts[0].published_at || posts[0].created_at
+    : null;
+
+  const staticEntries = [
+    {
+      loc: buildAbsoluteUrl('/'),
+      lastmod: toIsoDate(latestPostDate || Date.now()),
+      changefreq: 'weekly',
+      priority: '1.0'
+    },
+    {
+      loc: buildAbsoluteUrl('/blog'),
+      lastmod: toIsoDate(latestPostDate || Date.now()),
+      changefreq: 'daily',
+      priority: '0.9'
+    }
+  ];
+
+  const postEntries = posts.map((post) => ({
+    loc: buildAbsoluteUrl(`/blog/${encodeURIComponent(post.slug)}`),
+    lastmod: toIsoDate(post.updated_at || post.published_at || post.created_at),
+    changefreq: 'weekly',
+    priority: '0.8'
+  }));
+
+  const allEntries = [...staticEntries, ...postEntries];
+  const urlset = allEntries.map((entry) => `
+  <url>
+    <loc>${escapeXml(entry.loc)}</loc>
+    <lastmod>${escapeXml(entry.lastmod)}</lastmod>
+    <changefreq>${escapeXml(entry.changefreq)}</changefreq>
+    <priority>${escapeXml(entry.priority)}</priority>
+  </url>`).join('');
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urlset}
+</urlset>`;
+
+  res.set('Content-Type', 'application/xml; charset=utf-8');
+  res.send(xml);
+}));
+
+router.get('/robots.txt', (_req, res) => {
+  const robots = [
+    'User-agent: *',
+    'Allow: /',
+    'Disallow: /admin',
+    `Sitemap: ${buildAbsoluteUrl('/sitemap.xml')}`
+  ].join('\n');
+
+  res.set('Content-Type', 'text/plain; charset=utf-8');
+  res.send(robots);
+});
 
 module.exports = router;
